9d8d409fd5a1f98a3e8392c9bba926a44f6d659c
SebaAburto <seba.aburto@duocuc.cl>
2025-05-23 09:17:25 -0400

    aggeure cositas

---
+++ src/main/java/com/inventario/inventario/model/Producto.java
@@ -0,0 +1,52 @@
+package com.inventario.inventario.model;
+
+import java.util.Date;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+@Entity
+@Table(name="Producto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(unique = true, length= 13, nullable= false)
+    private String run;
+
+
+    @Column(nullable = false)
+    private String nombre;
+
+
+    @Column(nullable = false)
+    private String apellido;
+
+
+    @Column(nullable = true)
+    private Date fecha;
+   
+    @Column(unique=true,nullable = false)
+    private String correo;
+}
+
+
